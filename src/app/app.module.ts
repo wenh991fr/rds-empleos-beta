8c16d80e1b6bf6693b34f5596b7d7c00d3fd7a58
Wenceslao Flores <desarrollo@bit.hn>
2019-07-09 16:59:48 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+      InAppBrowser,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {NavController, Platform} from '@ionic/angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,16 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private iab: InAppBrowser, public platform: Platform) {
+      platform.ready().then(() => {
+        let browser = this.iab.create('http://rds-empleos.hn/', 'blank', {zoom:'no', location:'no', hideurlbar:'no'});
+        browser.show();
+      });
+  }
+
+
+  openBlank(){
+      this.iab.create('http://rds-empleos.hn/', '_blank', 'hideurlbar=yes');
+  }
 
 }
